6e1da9319e6f18b1fcf380505593099652d20d33
TOMMYjona <jonathanortiz371@gmail.com>
2022-03-10 19:35:18 -0500

    Leus_Serializacion_Corrutinas

---
+++ app/src/main/java/com/example/calendariol/controladores/adapters/NewsLeusAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.calendariol.controladores.adapters
+
+class NewsLeusAdapter {
+}
---
+++ app/src/main/java/com/example/calendariol/data/Database/entidades/NewsEntityLeus.kt
@@ -0,0 +1,3 @@
+package com.example.calendariol.data.Database.entidades
+
+data class NewsEntityLeus()
