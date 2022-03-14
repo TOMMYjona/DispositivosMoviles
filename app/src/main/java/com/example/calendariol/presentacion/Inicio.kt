3ab3599ff6ce2be28b2f55754c5e47b4b1c07df7
TOMMYjona <jonathanortiz371@gmail.com>
2022-03-13 21:07:27 -0500

    Leus_Google_Login_Add_Correction

--- app/src/main/java/com/example/calendariol/presentacion/Inicio.kt
+++ app/src/main/java/com/example/calendariol/presentacion/Inicio.kt
@@ -1,23 +1,36 @@
 package com.example.calendariol.presentacion
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import com.example.calendariol.R
 import com.example.calendariol.databinding.ActivityInicioBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_inicio.*
 
 class Inicio : AppCompatActivity() {
     private lateinit var binding: ActivityInicioBinding
+    //constante para google
+    private val GOOGLE_SIGN_IN=100
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        Thread.sleep(2000)//no es recomendable por el manejo de datos
-        setTheme(R.style.Theme_CalendarioL)
         super.onCreate(savedInstanceState)
         binding= ActivityInicioBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Thread.sleep(2000)//no es recomendable por el manejo de datos
+        setTheme(R.style.Theme_CalendarioL)
         //Analytics Event
         val anlytics= FirebaseAnalytics.getInstance(this)
         val bundle=Bundle()
@@ -25,7 +38,23 @@ class Inicio : AppCompatActivity() {
         anlytics.logEvent("InitScreen",bundle)
         //setup
         setup()
+        //verifica si existe una sesion activa
+        session()
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        auntlayout.visibility=View.VISIBLE
+    }
+    private fun session(){
+        val prefs=getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)//ficha de preferencias
+        val email=prefs.getString("email",null)
+        val provider=prefs.getString("provider",null)
+        if (email!=null && provider!=null){
+            auntlayout.visibility=View.INVISIBLE//se pone invesible si exite una session iniciada
+            showhome(email,ProviderType.valueOf(provider))
+        }
     }
     private fun setup(){
         title="Autentification"
@@ -58,7 +87,41 @@ class Inicio : AppCompatActivity() {
             }
 
         }
+        binding.GoogleButton.setOnClickListener(){
+            //configuracion
+            val googleconf=
+                GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build()
+
+            val googleclient=GoogleSignIn.getClient(this,googleconf)
+            googleclient.signOut()
+            //startActivityForResult(googleclient.signInIntent,GOOGLE_SIGN_IN)
+            getResult.launch(googleclient.signInIntent)
+        }
     }
+    private val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result->
+            if(result.resultCode == Activity.RESULT_OK){
+                val task=GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                try {
+                    val account=task.getResult(ApiException::class.java)
+                    if(account!=null){//si la cuenta es distinta de nula se recupera la credencial de firebase
+                        val credential=GoogleAuthProvider.getCredential(account.idToken,null)
+                        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener{
+                            if(it.isSuccessful){
+                                showhome(account.email?:"",ProviderType.GOOGLE)
+                            }else{
+                                showalert()
+                            }
+
+                        }
+                    }
+
+                }catch (e:ApiException){
+                    showalert()
+                }
+            }
+        }
 
     private fun showalert(){
         val builder=AlertDialog.Builder(this)
@@ -76,4 +139,5 @@ class Inicio : AppCompatActivity() {
         }
         startActivity(Mainintent)
     }
+
 }
